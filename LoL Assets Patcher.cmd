fde10a6ab0ab57c49cd97d26a45202607b002fcf
Loggan08 <ilja.korsun@gmail.com>
2014-04-03 16:53:06 +0200

    fix

--- LoL Assets Patcher.cmd
+++ LoL Assets Patcher.cmd
@@ -21,8 +21,8 @@ for /f "delims=" %%A in ('dir "<*>" /AD /O-D /B') do (
 chdir /d %LoL%\
 Powershell -executionpolicy Bypass -File "sources.ps1"
 for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
-if "%version%" == "6.*" COPY "%dbghelp.dll" "RADS\solutions\lol_game_client_sln\releases\%sln%\deploy\*" /y
-if "%version%" == "5.*" COPY "%dbghelpxp.dll" "RADS\solutions\lol_game_client_sln\releases\%sln%\deploy\dbghelp.dll" /y
+if "%version%" == "6.*" COPY "dbghelp.dll" "RADS\solutions\lol_game_client_sln\releases\%sln%\deploy\*" /y
+if "%version%" == "5.*" COPY "dbghelpxp.dll" "RADS\solutions\lol_game_client_sln\releases\%sln%\deploy\dbghelp.dll" /y
 COPY "cg.dll" "RADS\solutions\lol_game_client_sln\releases\%sln%\deploy\*" /y
 COPY "cgD3D9.dll" "RADS\solutions\lol_game_client_sln\releases\%sln%\deploy\*" /y
 COPY "cggl.dll" "RADS\solutions\lol_game_client_sln\releases\%sln%\deploy\*" /y
