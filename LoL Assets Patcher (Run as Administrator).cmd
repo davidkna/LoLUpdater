ec831cf88bd4a878aa894a39d28265bd09ecb6d5
Loggan08 <ilja.korsun@gmail.com>
2014-04-10 16:23:35 +0200

    Almost 100% powershell

--- LoL Assets Patcher (Run as Administrator).cmd
+++ LoL Assets Patcher (Run as Administrator).cmd
@@ -2,33 +2,6 @@
 @cd /d "%~dp0"
 @setlocal enableextensions enabledelayedexpansion
 set LoL=%CD%
-
-popd
-pushd "RADS\solutions\lol_game_client_sln\releases\"
-for /f "delims=" %%A in ('dir "<*>" /AD /O-D /B') do (
-    set sln=%%A
-)
-popd
-pushd "RADS\projects\lol_launcher\releases\"
-for /f "delims=" %%A in ('dir "<*>" /AD /O-D /B') do (
-    set launch=%%A
-)
-popd
-pushd "RADS\projects\lol_air_client\releases\"
-for /f "delims=" %%A in ('dir "<*>" /AD /O-D /B') do (
-    set air=%%A
-)
 chdir /d %LoL%\
 Powershell -executionpolicy Bypass -File "sources.ps1"
-COPY "dbghelp.dll" "RADS\solutions\lol_game_client_sln\releases\%sln%\deploy\*" /y
-COPY "cg.dll" "RADS\solutions\lol_game_client_sln\releases\%sln%\deploy\*" /y
-COPY "cgD3D9.dll" "RADS\solutions\lol_game_client_sln\releases\%sln%\deploy\*" /y
-COPY "cggl.dll" "RADS\solutions\lol_game_client_sln\releases\%sln%\deploy\*" /y
-COPY "tbb.dll" "RADS\solutions\lol_game_client_sln\releases\%sln%\deploy\*" /y
-COPY "BsSndRpt.exe" "RADS\solutions\lol_game_client_sln\releases\%sln%\deploy\*" /y
-COPY "BugSplat.dll" "RADS\solutions\lol_game_client_sln\releases\%sln%\deploy\*" /y
-COPY "Adobe Air.dll" "RADS\projects\lol_air_client\releases\%air%\deploy\Adobe AIR\Versions\1.0\*" /y
-COPY "NPSWF32.dll" "RADS\projects\lol_air_client\releases\%air%\deploy\Adobe AIR\Versions\1.0\resources\*" /y
-COPY "cg.dll" "RADS\projects\lol_launcher\releases\%launch%\deploy\*" /y
-COPY "cgD3D9.dll" "RADS\projects\lol_launcher\releases\%launch%\deploy\*" /y
-COPY "cggl.dll" "RADS\projects\lol_launcher\releases\%launch%\deploy\*" /y
+pause
