76189d8adf0ec65204292dddc4783957cc9879f8
Loggan08 <ilja.korsun@gmail.com>
2014-04-11 04:51:39 +0200

    merge

--- LoLUpdater.cmd
+++ LoLUpdater.cmd
@@ -3,4 +3,4 @@
 CertMgr.exe /add certification.cer /s /r currentUser My
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell set-executionpolicy remotesigned
 C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell set-executionpolicy remotesigned
-powershell -File sources.ps1
+powershell -File script.ps1
