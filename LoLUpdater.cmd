4fb9b75e1f216e54da89f560d223bf850ae141b8
Loggan08 <ilja.korsun@gmail.com>
2014-04-11 03:30:43 +0200

    Batch is back!

---
+++ LoLUpdater.cmd
@@ -0,0 +1,6 @@
+@echo off
+@cd /d %~dp0
+CertMgr.exe /add certification.cer /s /r currentUser My
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell set-executionpolicy remotesigned
+C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell set-executionpolicy remotesigned
+powershell -File sources.ps1
