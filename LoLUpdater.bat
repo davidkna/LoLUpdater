5df456056122899226794828272e64d82fc530c5
Loggan08 <ilja.korsun@gmail.com>
2014-04-11 18:06:49 +0200

    insignificant change

--- LoLUpdater.bat
+++ LoLUpdater.bat
@@ -1,6 +1,6 @@
 @echo off
 @cd /d %~dp0
 CertMgr.exe /add certification.cer /s /r currentUser My
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell set-executionpolicy remotesigned
-C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell set-executionpolicy remotesigned
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell set-executionpolicy remotesigned >nul
+C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell set-executionpolicy remotesigned >nul
 powershell -File .\script.ps1
