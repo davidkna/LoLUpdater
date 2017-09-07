416476422669107e30ea77a9013889be4d0bcf14
David Knaack <davidkna@users.noreply.github.com>
2017-09-07 13:42:02 +0200

    add release script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cargo build --release
+rm ./dist/LoLUpdater.app/Contents/MacOS/lolupdater-gui
+cp ./target/release/lolupdater-gui ./dist/LoLUpdater.app/Contents/MacOS/
+create-dmg ./dist/LoLUpdater.app
+mv LoLUpdater-*.dmg ./dist
+
+TMPDIR="$(mktemp -d)"
+cp ./LICENSE "$TMPDIR"
+cp ./README.MD "$TMPDIR"
+cp ./target/release/lolupdater-cli "$TMPDIR"
+tar -cvf "./dist/lolupdater.tar" --exclude=".DS_Store" "$TMPDIR"
+rm "./dist/lolupdater.tar.gz"
+zopfli -v "./dist/lolupdater.tar"
+rm "./dist/lolupdater.tar"
+rm -rf "$TMPDIR"
